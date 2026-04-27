8f9f41830a0690bbfeede0d59ad03f7001fd5097
Bailey <baileykelly0918@gmail.com>
2026-04-27 14:06:48 -0400

    Fixed errors

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
